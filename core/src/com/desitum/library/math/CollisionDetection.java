5c3434c8fa5efaa097e4d3c9451f8c85bbe93c1f
Dakota Van Ry <dakota.vanry@trudigital.net>
2016-09-16 17:56:58 -0600

    CollisionDetection now includes detection for mouse/touch in widget.

--- core/src/com/desitum/library/math/CollisionDetection.java
+++ core/src/com/desitum/library/math/CollisionDetection.java
@@ -4,11 +4,16 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.desitum.library.game_objects.GameObject;
+import com.desitum.library.widgets.Widget;
 
 /**
  * Created by Zmyth97 on 2/9/2015.
  */
 public class CollisionDetection {
+
+    public static boolean pointInWidget(Widget widget, Vector3 point) {
+        return pointInRectangle(widget.getBoundingRectangle(), point);
+    }
 
     public static boolean pointInRectangle(Rectangle rect, Vector3 point) {
         float rw, rh, rx, ry, px, py;
